f7ce5b52964ee99fbc72c94d537a47709c94a982
Kamil2433 <136186707+Kamil2433@users.noreply.github.com>
2023-08-22 20:42:29 +0530

    Update Sockerprovider.js

--- Client/justchat/src/context/Sockerprovider.js
+++ Client/justchat/src/context/Sockerprovider.js
@@ -12,7 +12,7 @@ export function SocketProvider({ id, children }) {
 
   useEffect(() => {
     const newSocket = io(
-      'http://localhost:5000',
+      'https://justchat2.onrender.com',
       { query: { id } },
      
       {cors:'*'}
