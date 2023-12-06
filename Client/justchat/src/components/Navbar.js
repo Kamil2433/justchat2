5aa441b40d626a9929dfc795a0d5e1943826d938
Kamil2433 <136186707+Kamil2433@users.noreply.github.com>
2023-12-06 20:52:24 +0530

    Added Logo

--- Client/justchat/src/components/Navbar.js
+++ Client/justchat/src/components/Navbar.js
@@ -24,7 +24,7 @@ export default function Navbar({id}) {
 
   return (
     <div className='sticky-top'>
-        <div className="float-start  ms-5 mt-3 h2 ">JustChat.com</div>
+        <div className="float-start  ms-5 mt-3 h2 "><i class="fa-solid fa-comment" style={{color:"#588adf"}}></i>JustChat.com</div>
         <div className="float-end mt-3">
           <span className=" mt-2   w-auto ms-3"><b>Your Id: </b>{id}</span>
           <Button className=" bi bi-clipboard ms-2 " onClick={copyToClipboard}>
